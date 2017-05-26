6c19dc915989133d4b603191884dfac20bd6cca7
Mark Reid <contact@markreid.com.au>
2017-05-26 22:38:12 +1000

    Touch model and query helpers

---
+++ models/touch.js
@@ -0,0 +1,26 @@
+/**
+ * Touch model.
+ */
+
+module.exports = (sequelize, DataTypes) => sequelize.define('Touch', {
+  id: {
+    type: DataTypes.INTEGER,
+    unique: true,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  cardId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  timestamp: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  synced: {
+    type: DataTypes.DATE,
+    allowNull: true,
+    defaultValue: null,
+  },
+});
